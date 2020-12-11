25970f5d92572b7c51fd943b68f08e8d2c89c3ff
Wooki <74518085+sbw210@users.noreply.github.com>
2020-12-11 16:12:01 +0900

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,9 +1,39 @@
 #!/bin/bash
 
+##apache start
 source /etc/apache2/envvars
 /usr/sbin/apache2
 
-/usr/sbin/sshd -D
+
+##ssh start
+/usr/sbin/sshd
+
+
+##mysql start
+if [ -z $MYSQL_ROOT_PASSWORD ]; then
+  exit 1
+fi
+
+mysql_install_db --user mysql > /dev/null
+
+cat > /tmp/sql <<EOF
+
+USE mysql;
+
+FLUSH PRIVILEGES;
+
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+
+UPDATE user SET password=PASSWORD("$MYSQL_ROOT_PASSWORD") WHERE user='root';
+
+EOF
+
+mysqld --bootstrap --verbose=0 < /tmp/sql
+
+rm -rf /tmp/sql
+
+mysqld
+
 
 while true;
   do echo "still live";
